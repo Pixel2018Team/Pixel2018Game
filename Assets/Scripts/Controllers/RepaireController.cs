e4eb9862ff8828c6ff289128d56bd12f2f97532b
Samuel <carriere911@gmail.com>
2018-04-05 23:08:38 -0400

    add the basic controller for the broken object

--- Assets/Scripts/Controllers/RepaireController.cs
+++ Assets/Scripts/Controllers/RepaireController.cs
@@ -1,16 +1,46 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using Global;
 using UnityEngine;
 
-public class RepaireController : MonoBehaviour {
+public class RepaireController : MonoBehaviour
+{
+    private readonly string GONZUELA = "gonzuela";
+    private bool isActive = false;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public float timeToRepaire = 5.0f;
+
+    public InputMapping.PlayerTag playerTag;
+
+    void Start()
+    {
+
+    }
+
+    public bool isBroken()
+    {
+        return timeToRepaire > 0;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == GONZUELA)
+        {
+            isActive = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == GONZUELA)
+        {
+            isActive = false;
+        }
+    }
+
+    void Update()
+    {
+        if (isActive && isBroken() && Input.GetButton(InputMapping.GetInputName(playerTag, InputMapping.Input.X)))
+        {
+            timeToRepaire -= Time.deltaTime;
+        }
+    }
 }
