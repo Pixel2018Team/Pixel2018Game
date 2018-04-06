002b6c76803f21e1186b12a4fcfdfad12ec4985d
DarkXolotl <ghis7@hotmail.com>
2018-04-06 03:43:41 -0400

    Corrected Door Script
    Removed the erronous singleton from the Door script which was also renamed "DoorScript"

--- Assets/Scripts/Controllers/DoorScript.cs
+++ Assets/Scripts/Controllers/DoorScript.cs
@@ -2,14 +2,6 @@
 
 public class DoorManager : MonoBehaviour
 {
-    public static DoorManager Instance = null;
-
-    // Singleton logic
-    private void Awake()
-    {
-        if (Instance == null) Instance = this;
-        else if (Instance != this) Destroy(this);
-    }
 
     /// <summary>
     /// Do not use this method to close the door! Use Interact() instead.
