5d9e9e5c946034724804a37b27c185ef0d0f1c97
Samuel <carriere911@gmail.com>
2018-04-06 07:41:08 -0400

    add base kidcontroller with the tag option

---
+++ Assets/Scripts/Controllers/GonzuelaController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class GonzuelaController : MonoBehaviour
+{
+    private string KID_TAG = "kid";
+
+    public float radius = 10.0f;
+
+    void Start()
+    {
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+
+    void Update()
+    {
+        ScanKidsInReach();
+    }
+
+    private void ScanKidsInReach()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, radius);
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            if (colliders[i].tag == KID_TAG)
+            {
+                var kidController = colliders[i].GetComponent<KidController>();
+                if (kidController.isBad && !kidController.isTag)
+                {
+                    kidController.GetTagged();
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Controllers/KidController.cs
@@ -0,0 +1,35 @@
+﻿using Global;
+using UnityEngine;
+
+public class KidController : MonoBehaviour
+{
+    private SpriteRenderer _tag;
+
+    public bool isBad = false;
+    public bool isTag = false;
+    public InputMapping.PlayerTag playerTag;
+
+    void Start()
+    {
+        _tag = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    public void GetTagged()
+    {
+        isTag = true;
+        _tag.enabled = true;
+        // Play animation
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown(InputMapping.GetInputName(playerTag, InputMapping.Input.Y)))
+        {
+            isBad = true;
+        }
+        else if (Input.GetButtonUp(InputMapping.GetInputName(playerTag, InputMapping.Input.Y)))
+        {
+            isBad = false;
+        }
+    }
+}
