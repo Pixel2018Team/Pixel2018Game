cfb7d75177da7b0d0b524492c84b720658e43ab9
yannismrad <yannismrad@free.fr>
2018-04-06 11:23:19 -0400

    restore top down controller

---
+++ Assets/Scripts/Controllers/TopDownController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using Global;
+
+public class TopDownController : MonoBehaviour
+{
+    public float speed = 6.0F;
+    public InputMapping.PlayerTag playerTag;
+
+    private Rigidbody _rigidBody;
+    private Vector3 _moveInput;
+    private Vector3 _moveVelocity;
+    private Animator _animator;
+
+    void Start()
+    {
+        _rigidBody = GetComponent<Rigidbody>();
+        _animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        Debug.DrawLine(transform.position, transform.position + transform.rotation * Vector3.forward * 3, Color.red);
+
+        _moveInput = new Vector3(Input.GetAxisRaw(InputMapping.GetInputName(playerTag, InputMapping.Input.Horizontal)), 0f,
+            Input.GetAxisRaw(InputMapping.GetInputName(playerTag, InputMapping.Input.Vertical)));
+        _moveVelocity = _moveInput * speed;
+
+        Vector3 newDirection = Vector3.right * Input.GetAxisRaw(InputMapping.GetInputName(playerTag, InputMapping.Input.Horizontal))
+            + Vector3.forward * Input.GetAxisRaw(InputMapping.GetInputName(playerTag, InputMapping.Input.Vertical));
+        if (newDirection.sqrMagnitude > 0.0f)
+        {
+            transform.rotation = Quaternion.LookRotation(newDirection, Vector3.up);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        _animator.SetFloat("speed", _moveVelocity.magnitude);
+        _rigidBody.velocity = _moveVelocity + _rigidBody.velocity.y * Vector3.up;
+    }
+}
