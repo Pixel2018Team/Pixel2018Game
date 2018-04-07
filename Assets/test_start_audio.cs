c4b1427cf7bc5157d2816c0f69c493b0e28f8f1f
Samuel <carriere911@gmail.com>
2018-04-07 13:19:10 -0400

    update sound

---
+++ Assets/test_start_audio.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test_start_audio : MonoBehaviour
+{
+    void Start()
+    {
+        AkSoundEngine.PostEvent("Gamestart", gameObject);
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("P1_A"))
+        {
+            LevelManager.Instance.AddChaos(-25);
+        }
+    }
+}
